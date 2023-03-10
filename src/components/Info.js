626c96b0320008bc6ea2442a95ca43fbdf4460b6
Sean Eric Thomas <set@seanericthomas.com>
2023-03-10 12:32:13 -0800

    Switch between editing and displaying general info

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
 export default class Info extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'My Name',
+      email: 'email@example.com',
+      phone: '123-456-7890',
+    };
+  }
+
   render() {
+    const { mode } = this.props;
+    const { name, email, phone } = this.state;
+
     return (
       <div>
         <h2>Info:</h2>
         <p>
-          <span>My Name</span>
+          {mode === 'edit' ? (
+            <input type="text" placeholder={name} />
+          ) : (
+            <span>{name}</span>
+          )}
         </p>
         <p>
-          <span>email@example.com</span>
-          <span>123-456-7890</span>
+          {mode === 'edit' ? (
+            <input type="email" placeholder={email} />
+          ) : (
+            <span>{email}</span>
+          )}
+          {mode === 'edit' ? (
+            <input type="tel" placeholder={phone} />
+          ) : (
+            <span>{phone}</span>
+          )}
         </p>
       </div>
     );
