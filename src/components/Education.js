4c6e7fb4ecc6aafa278101b8686824701be005d0
Sean Eric Thomas <set@seanericthomas.com>
2023-03-10 12:48:55 -0800

    Switch between editing and displaying education info

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,14 +1,54 @@
 import React from 'react';
 
 export default class Education extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      degree: 'Degree Title',
+      school: 'School Name',
+      dates: '2005 - 2009',
+      info: 'Additional information',
+    };
+  }
+
   render() {
+    const { mode } = this.props;
+    const {
+      degree, school, dates, info,
+    } = this.state;
+
     return (
       <div>
         <h2>Education:</h2>
-        <p><span>Degree Title</span></p>
-        <p><span>School Name</span></p>
-        <p><span>Dates Attended</span></p>
-        <p><span>Additional Info</span></p>
+        <p>
+          {mode === 'edit' ? (
+            <input type="text" placeholder={degree} />
+          ) : (
+            <span>{degree}</span>
+          )}
+        </p>
+        <p>
+          {mode === 'edit' ? (
+            <input type="text" placeholder={school} />
+          ) : (
+            <span>{school}</span>
+          )}
+        </p>
+        <p>
+          {mode === 'edit' ? (
+            <input type="text" placeholder={dates} />
+          ) : (
+            <span>{dates}</span>
+          )}
+        </p>
+        <p>
+          {mode === 'edit' ? (
+            <input type="text" placeholder={info} />
+          ) : (
+            <span>{info}</span>
+          )}
+        </p>
       </div>
     );
   }
