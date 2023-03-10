8c63898757f7213dfb80933c577f583682ac6ce4
Sean Eric Thomas <set@seanericthomas.com>
2023-03-10 13:20:42 -0800

    Edit general information

--- src/components/Info.js
+++ src/components/Info.js
@@ -9,6 +9,28 @@ export default class Info extends React.Component {
       email: 'email@example.com',
       phone: '123-456-7890',
     };
+
+    this.changeName = this.changeName.bind(this);
+    this.changeEmail = this.changeEmail.bind(this);
+    this.changePhone = this.changePhone.bind(this);
+  }
+
+  changeName(event) {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  changeEmail(event) {
+    this.setState({
+      email: event.target.value,
+    });
+  }
+
+  changePhone(event) {
+    this.setState({
+      phone: event.target.value,
+    });
   }
 
   render() {
@@ -20,19 +42,19 @@ export default class Info extends React.Component {
         <h2>Info:</h2>
         <p>
           {mode === 'edit' ? (
-            <input type="text" placeholder={name} />
+            <input type="text" value={name} onChange={this.changeName} />
           ) : (
             <span>{name}</span>
           )}
         </p>
         <p>
           {mode === 'edit' ? (
-            <input type="email" placeholder={email} />
+            <input type="email" value={email} onChange={this.changeEmail} />
           ) : (
             <span>{email}</span>
           )}
           {mode === 'edit' ? (
-            <input type="tel" placeholder={phone} />
+            <input type="tel" value={phone} onChange={this.changePhone} />
           ) : (
             <span>{phone}</span>
           )}
