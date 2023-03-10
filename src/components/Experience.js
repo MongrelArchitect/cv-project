cae4fa887b6601f78fd760f1649cb2fdc6dbb742
Sean Eric Thomas <set@seanericthomas.com>
2023-03-10 12:55:17 -0800

    Switch between editing and displaying experience info

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,14 +1,54 @@
 import React from 'react';
 
 export default class Experience extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      company: 'Company Name',
+      title: 'Job Title',
+      duties: 'Job Duties',
+      dates: '01/01/2010 - 12/12/2020',
+    };
+  }
+
   render() {
+    const { mode } = this.props;
+    const {
+      company, title, duties, dates,
+    } = this.state;
+
     return (
       <div>
         <h2>Experience:</h2>
-        <p><span>Company Name</span></p>
-        <p><span>Job Title</span></p>
-        <p><span>Job Duties</span></p>
-        <p><span>Dates</span></p>
+        <p>
+          {mode === 'edit' ? (
+            <input type="text" placeholder={company} />
+          ) : (
+            <span>{company}</span>
+          )}
+        </p>
+        <p>
+          {mode === 'edit' ? (
+            <input type="text" placeholder={title} />
+          ) : (
+            <span>{title}</span>
+          )}
+        </p>
+        <p>
+          {mode === 'edit' ? (
+            <input type="text" placeholder={duties} />
+          ) : (
+            <span>{duties}</span>
+          )}
+        </p>
+        <p>
+          {mode === 'edit' ? (
+            <input type="text" placeholder={dates} />
+          ) : (
+            <span>{dates}</span>
+          )}
+        </p>
       </div>
     );
   }
