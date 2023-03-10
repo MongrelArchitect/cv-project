704ece9a0ae574af70ea149dd2bc1b822c2abc89
Sean Eric Thomas <set@seanericthomas.com>
2023-03-10 12:17:09 -0800

    Enable mode buttons based on state

--- src/App.js
+++ src/App.js
@@ -5,13 +5,38 @@ import Education from './components/Education';
 import Experience from './components/Experience';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      mode: 'edit',
+    };
+
+    this.changeMode = this.changeMode.bind(this);
+  }
+
+  changeMode() {
+    const { mode } = this.state;
+    if (mode === 'edit') {
+      this.setState({
+        mode: 'display',
+      });
+    } else {
+      this.setState({
+        mode: 'edit',
+      });
+    }
+  }
+
   render() {
+    const { mode } = this.state;
+
     return (
       <div>
-        <Mode />
-        <Info />
-        <Education />
-        <Experience />
+        <Mode mode={mode} changeMode={this.changeMode} />
+        <Info mode={mode} />
+        <Education mode={mode} />
+        <Experience mode={mode} />
       </div>
     );
   }
--- src/components/Mode.js
+++ src/components/Mode.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 export default class Mode extends React.Component {
   render() {
+    const { mode } = this.props;
+    const { changeMode } = this.props;
+
     return (
       <div>
         <h2>Mode:</h2>
-        <button type="button" id="edit">EDIT</button>
-        <button type="button" id="display">DISPLAY</button>
+        <button type="button" id="edit" disabled={mode === 'edit'}>
+          EDIT
+        </button>
+        <button type="button" id="display" disabled={mode !== 'edit'}>
+          DISPLAY
+        </button>
       </div>
     );
   }
