ef6f153a4c77ac6d051580928aaebb4a5fb2e47e
Sean Eric Thomas <set@seanericthomas.com>
2023-03-11 21:49:12 -0800

    Edit and delete individual entries in "education" section

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,50 +5,157 @@ export default class Education extends React.Component {
     super();
 
     this.state = {
-      degree: 'Degree Title',
-      school: 'School Name',
-      dates: '2005 - 2009',
-      info: 'Additional information',
+      education: [
+        {
+          degree: 'Degree Title',
+          school: 'School Name',
+          dates: '2005 - 2009',
+          info: 'Additional information',
+        },
+        {
+          degree: 'Some Certification',
+          school: 'Another School',
+          dates: '2011 - 20013',
+          info: 'Some more details about my cert',
+        },
+      ],
     };
+
+    this.changeDegree = this.changeDegree.bind(this);
+    this.changeSchool = this.changeSchool.bind(this);
+    this.changeDates = this.changeDates.bind(this);
+    this.changeInfo = this.changeInfo.bind(this);
+    this.delete = this.delete.bind(this);
+  }
+
+  getEduCopy() {
+    const { education } = this.state;
+    const tempEdu = [];
+    education.forEach((edu) => {
+      const eduCopy = {};
+      Object.keys(edu).forEach((key) => {
+        eduCopy[key] = edu[key];
+      });
+      tempEdu.push(eduCopy);
+    });
+    return tempEdu;
+  }
+
+  changeDegree(event) {
+    const { index } = event.target.dataset;
+    const tempEdu = this.getEduCopy();
+    tempEdu[index].degree = event.target.value;
+    this.setState({
+      education: tempEdu,
+    });
+  }
+
+  changeSchool(event) {
+    const { index } = event.target.dataset;
+    const tempEdu = this.getEduCopy();
+    tempEdu[index].school = event.target.value;
+    this.setState({
+      education: tempEdu,
+    });
+  }
+
+  changeDates(event) {
+    const { index } = event.target.dataset;
+    const tempEdu = this.getEduCopy();
+    tempEdu[index].dates = event.target.value;
+    this.setState({
+      education: tempEdu,
+    });
+  }
+
+  changeInfo(event) {
+    const { index } = event.target.dataset;
+    const tempEdu = this.getEduCopy();
+    tempEdu[index].info = event.target.value;
+    this.setState({
+      education: tempEdu,
+    });
+  }
+
+  delete(event) {
+    const tempEdu = this.getEduCopy();
+    const { index } = event.target.dataset;
+    this.setState({
+      education: tempEdu.filter((edu) => tempEdu.indexOf(edu) !== +index),
+    });
   }
 
   render() {
     const { mode } = this.props;
-    const {
-      degree, school, dates, info,
-    } = this.state;
+    const { education } = this.state;
 
     return (
       <div>
         <h2>Education:</h2>
-        <p>
-          {mode === 'edit' ? (
-            <input type="text" placeholder={degree} />
-          ) : (
-            <span>{degree}</span>
-          )}
-        </p>
-        <p>
-          {mode === 'edit' ? (
-            <input type="text" placeholder={school} />
-          ) : (
-            <span>{school}</span>
-          )}
-        </p>
-        <p>
-          {mode === 'edit' ? (
-            <input type="text" placeholder={dates} />
-          ) : (
-            <span>{dates}</span>
-          )}
-        </p>
-        <p>
-          {mode === 'edit' ? (
-            <input type="text" placeholder={info} />
-          ) : (
-            <span>{info}</span>
-          )}
-        </p>
+        {mode === 'edit' ? (
+          <div>
+            <button type="button"> + </button>
+            <hr />
+          </div>
+        ) : null}
+        {education.map((edu) => {
+          const key = education.indexOf(edu);
+          if (mode === 'edit') {
+            return (
+              <div key={key}>
+                <h3>
+                  <input
+                    type="text"
+                    value={edu.degree}
+                    data-index={education.indexOf(edu)}
+                    onChange={this.changeDegree}
+                  />
+                </h3>
+                <p>
+                  <input
+                    type="text"
+                    value={edu.school}
+                    data-index={education.indexOf(edu)}
+                    onChange={this.changeSchool}
+                  />
+                </p>
+                <p>
+                  <input
+                    type="text"
+                    value={edu.dates}
+                    data-index={education.indexOf(edu)}
+                    onChange={this.changeDates}
+                  />
+                </p>
+                <p>
+                  <textarea
+                    value={edu.info}
+                    data-index={education.indexOf(edu)}
+                    onChange={this.changeInfo}
+                  />
+                </p>
+                <button
+                  type="button"
+                  data-index={education.indexOf(edu)}
+                  onClick={this.delete}
+                >
+                  DELETE
+                </button>
+                <hr />
+              </div>
+            );
+          }
+
+          return (
+            <div key={key}>
+              <h3>{edu.degree}</h3>
+              <p>{edu.school}</p>
+              <p>{edu.dates}</p>
+              <p>{edu.info}</p>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     );
   }
